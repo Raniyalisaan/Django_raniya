0dfb76697c3c4ea2aa3f46dd9e2dc4e49aab8488
Raniya Lisaan <raniyalisaan1331@gmail.com>
2026-07-21 13:22:14 +0530

    Updated in urls.py

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from new_app import views
 
 urlpatterns = [
-path("test",views.test,name='test')
+path("test",views.test,name='test'),
+path("test1",views.test_html,name='test1')
+
+
 ]
